93fdbdfff691df94614acf727e8ec4941381a144
Vincent Cote <vincentcote@Vincents-iMac.lb.shawcable.net>
2017-06-01 15:41:55 -0600

    comented out unused line cause the unit test to throw a warning.

--- test/unit/specs/Deck.spec.js
+++ test/unit/specs/Deck.spec.js
@@ -88,7 +88,7 @@ describe('Deck.js', () => {
     let testDeck = new Deck()
     testDeck.initDeck()
     let testCard = new Card(0, 1, 'I')
-    let drawnCard = testDeck.draw()
+    // let drawnCard = testDeck.draw()
     expect(testDeck.cards.length).to.equal(71)
     expect(typeof drawnCard).to.equal('object')
     // The deck shuffles itself when created, no way of know what the top card is
