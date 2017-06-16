2ca253dedf8d748e1be5b11b06604ac7288886d1
vincentcote87 <vincentcote87@gmail.com>
2017-06-16 10:57:59 -0600

    Removed debugging line

--- src/components/SettingsComponent.vue
+++ src/components/SettingsComponent.vue
@@ -23,7 +23,6 @@
             <div id="scoreSelect">
             Score to Win:
             <select class="custom-select" name="select" v-model="selected">
-              <option value="1">TESTING</option>
               <option value="10">Short (score 10)</option>
               <option value="20">Medium (score 20)</option>
               <option value="30">Long (score 30)</option>
