be434920ff5187ce95c9e62f11c5f47a1af6e4ed
vincentcote87 <vincentcote87@gmail.com>
2017-06-26 13:07:22 -0600

    Added a line showing the winning score

--- src/components/PlayerInfoPanel.vue
+++ src/components/PlayerInfoPanel.vue
@@ -36,6 +36,11 @@
       </div>
       <div class="container" style="border-top: 1px solid white; padding: 10px;">
         <div class="row">
+          <div class="col-md-12">
+            <h4>Score To Win is: <b>{{ $store.getters.getScoreLimit }}</b></h4>
+          </div>
+        </div>
+        <div class="row">
           <div :class="colSize" v-for="player in players" style="text-align: left">
             <div style="float: left; margin-right: 10px;"><h4><b><a @click="openModal" style="cursor: pointer; color: rgba(10,1,1,0.79);">{{ player.name }}:</a></b></h4></div>
               <div> True Score: {{ player.trueScore }} <br> False Score: {{ player.falseScore }}</div>
