5cae05ea9bfe5af5115bb83bb40728a5fe96d015
vincentcote87 <vincentcote87@gmail.com>
2017-07-19 10:36:58 -0600

    Changed routing to work in Firefox

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -17,7 +17,7 @@
         <label class="checkbox-inline"><input type="checkbox" value="true" v-model="factsToggle" checked>FUN FACTS</label>
         </div>
         <div id="header-buttons">
-        <button class="btn btn-primary"><router-link to="/" style="text-decoration: none">New Game</router-link></button>
+        <button class="btn btn-primary" @click="() => {this.$router.push('/')}">New Game</button>
         <button class="btn btn-primary" data-toggle="modal" data-target=".rules">Rules</button>
         <button class="btn btn-primary" data-toggle="modal" data-target=".credits">Credits</button>
         <a class="btn btn-primary" href="https://github.com/johnanvik/program-wars/issues/new" target="_blank">Report Issue</a>
