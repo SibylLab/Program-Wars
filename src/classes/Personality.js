ff265ab1be11a20bdce6a9e829257fd2c1bc9d33
vincentcote87 <vincentcote87@gmail.com>
2017-08-03 16:35:37 -0600

    removed debug

--- src/classes/Personality.js
+++ src/classes/Personality.js
@@ -6,18 +6,12 @@ export default class Personality {
     let num = Math.floor((Math.random() * 3) + 1);
     switch(num) {
       case 1:
-      case 2: this.personality = new Sprinter(); console.log('num was ' + num); break;
-      case 3: this.personality = new Hacker(); console.log('num was ' + num); break;
+      case 2: this.personality = new Sprinter(); break;
+      case 3: this.personality = new Hacker(); break;
     }
     if(num === 1) {
       this.isTimid = true;
     }
-    // if(name === 'Flash' || name === 'JarJarBinks') {
-    //   this.personality = new Sprinter();
-    // }
-    // if(name === 'Joker') {
-    //   this.personality = new Hacker();
-    // }
   };
 
   turnLogic(event) {
