3ed860e510d89d72fdb553e4464468b546175a05
JaceRiehl <jace.riehl@gmail.com>
2018-05-17 11:17:09 -0600

    Adding npm-shrinkwrap to the .gitignore, its a substitute for package-lock.json but we dont need it.

--- src/components/RulesModal.vue
+++ src/components/RulesModal.vue
@@ -69,7 +69,7 @@
                       <div>
                       <ol>
                         <li>Group cards let you combine stacks of instructions and group cards into one card.</li>
-                        <li>The combined total of the instructions must be the same as the value of the group card.
+                        <li>The combined total of the stacks must be the same as the value of the group card.
                         <li>Group cards protect your instructions from Hack cards.</li>
                       </ol>
                       </div>
