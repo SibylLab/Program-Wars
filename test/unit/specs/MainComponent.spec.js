40555044b346129e0c382bf9fd0cd7ff88c9f7a8
JaceRiehl <jace.riehl@gmail.com>
2018-06-21 17:18:39 -0600

    Adding some tests

--- test/unit/specs/MainComponent.spec.js
+++ test/unit/specs/MainComponent.spec.js
@@ -1,23 +1,45 @@
-// // import mainComponent from '../../../src/components/MainComponent.vue'
-// //
-// // function getSubmit (Component, propsData) {
-// //   const Ctor = Vue.extend(Component)
-// //   const vm = new Ctor({ propsData }).$mount()
-// //   return vm.submit
-// // }
-// //
-// // describe('MainComponent.js', () => {
-// //   it('testing the submit funtion', () => {
-// //     let newPlayer = 'jane'
-// //     getSubmit()
-// //   })
-// // })
-//
-// import { store } from '../../../src/store/store'
-// import { actions } from '../../../src/store/actions'
-//
-// describe('store tests', () => {
-//   it('testing store', () => {
-//     expect(store.state.activeSide).to.equal(true)
-//   })
-// })
+import MainComponent from '../../../src/components/MainComponent.vue'
+import { store } from '../../../src/store/store'
+import { actions } from '../../../src/store/actions'
+
+// function getSubmit (Component, propsData) {
+//   const Ctor = Vue.extend(Component)
+//   const vm = new Ctor({ propsData }).$mount()
+//   return vm.submit
+// }
+
+describe('MainComponent.js', () => {
+  it('has a created hook', () => {
+    expect(typeof MainComponent.created).toBe('function')
+  })
+
+  it('sets the correct default data', () => {
+    expect(typeof MainComponent.data).toBe('function')
+    const defaultData = MyComponent.data()
+    expect(defaultData.idCounter).toBe(0)
+    expect(defaultData.dataToggle).toBe(false)
+    expect(defaultData.localPlayers).toBe([])
+    expect(defaultData.gameStart).toBe(false)
+    expect(defaultData.showDismissCards).toBe(false)
+    expect(defaultData.modalCards).toBe([])
+    expect(defaultData.gameOverWinner).toBe('')
+    expect(defaultData.gameOverText).toBe('')
+    expect(defaultData.modalId).toBe('gameOverModal')
+    expect(defaultData.tipsToggle).toBe(true)
+    expect(defaultData.factsToggle).toBe(true)
+    expect(defaultData.playerList).toBe([])
+    expect(defaultData.winner).toBe('')
+    expect(defaultData.winnerScore).toBe(0)
+    expect(defaultData.deleteData).toBe([])
+  })
+  // it('testing the submit funtion', () => {
+  //   let newPlayer = 'jane'
+  //   getSubmit()
+  // })
+})
+
+describe('store tests', () => {
+  it('testing store', () => {
+    expect(store.state.activeSide).to.equal(true)
+  })
+})
