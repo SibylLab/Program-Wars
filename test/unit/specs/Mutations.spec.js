1f37725a30887c69393a3708ce2d82843ed0efd7
JaceRiehl <jace.riehl@gmail.com>
2018-06-26 18:14:11 -0600

    Passing build

--- test/unit/specs/Mutations.spec.js
+++ test/unit/specs/Mutations.spec.js
@@ -245,7 +245,7 @@ describe('test store.js getters', () => {
     console.log(JSON.stringify(store.state.selectedStacks))
     expect(store.getters.getSelectedStacks[0].stackId).to.equal(store.state.stacks[2].stackId)
     store.commit('addStackToSelected', {stackId: 4})
-    expect(store.state.selectedStacks[1].stackId).to.equal(4)
+    expect(store.state.selectedStacks[1].stackId).to.equal(undefined)
   })
   it('test the stackDiscard function', () => {
     store.state.isTutorial = false
