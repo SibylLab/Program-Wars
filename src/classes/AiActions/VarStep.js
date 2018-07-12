63c9c4f89937aa1354f36468a6ef17b2d8caa81b
JaceRiehl <jace.riehl@gmail.com>
2018-07-12 17:09:09 -0600

    Testing some behaviours.

--- src/classes/AiActions/VarStep.js
+++ src/classes/AiActions/VarStep.js
@@ -2,6 +2,9 @@ import {Turn} from './AiChain'
 
 export default class Variable extends Turn {
   execute () {
+    console.log('card ' + this.hand.bestVCard)
+    console.log('stackToAddvariabel: ' + this.move.stackToAddVariable(this.event))
+    console.log('stacks: ' + this.event.stack.find(stack => stack.boolSide === this.boolSide))
     if (this.hand.bestVCard !== undefined && this.move.stackToAddVariable(this.event) !== undefined && this.event.stack.find(stack => stack.boolSide === this.boolSide)) {
       this.cardToPlay = this.hand.bestVCard
       this.stackToPlay = this.move.stackToAddVariable(this.event)
