c475a24c5108e62e41a2b3a9f5163cc469a6c421
JaceRiehl <jace.riehl@gmail.com>
2018-07-12 14:29:25 -0600

    This might have it.

--- src/classes/AiActions/DiscardStep.js
+++ src/classes/AiActions/DiscardStep.js
@@ -2,7 +2,6 @@ import {Turn} from './AiChain'
 
 export default class Discard extends Turn {
   execute () {
-    console.log(JSON.stringify(this.event.hand.cards))
     this.cardToPlay = this.event.hand.cards[0]
     this.moveType = 'discard'
     return true
