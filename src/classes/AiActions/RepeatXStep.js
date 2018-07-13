6139ddfe39788a53b70b8216652a7b67d553f1cf
JaceRiehl <jace.riehl@gmail.com>
2018-07-12 18:35:47 -0600

    Hoping this changes it

--- src/classes/AiActions/RepeatXStep.js
+++ src/classes/AiActions/RepeatXStep.js
@@ -2,7 +2,11 @@ import {Turn} from './AiChain'
 
 export default class RepeatX extends Turn {
   execute () {
+    console.log('rX card: ' + JSON.stringify(this.hand.rXCard))
+    console.log('stackToRepeat: ' + JSON.stringify(this.move.getStackToRepeat(this.event)))
+    console.log('stack to play ' + this.event.stack.find(stack => stack.boolSide === this.boolSide))
     if (this.hand.rXCard !== undefined && this.move.getStackToRepeat(this.event) !== undefined && this.event.stack.find(stack => stack.boolSide === this.boolSide)) {
+      console.log('in Repeat')
       this.cardToPlay = this.hand.rXCard
       this.stackToPlay = this.move.getStackToRepeat(this.event)
       this.moveType = 'play'
