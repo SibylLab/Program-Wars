0ef800db88388bd270d0cb18ff7e3a470b3a3c20
Dr. Anvik <john.anvik@uleth.ca>
2019-07-24 13:49:24 -0600

    Change label to "Instructions" on meter

--- src/components/MainGame/PlayerInfoPanel.vue
+++ src/components/MainGame/PlayerInfoPanel.vue
@@ -13,7 +13,7 @@
           <div style="float: left; margin-right: 10px;"><h4><b><a @click="openModal" style="cursor: pointer; color: rgba(10,1,1,0.79); font-size: 17px; -webkit-align-items: center " :style="pIPTextColour()">{{ player.name }}:</a></b></h4></div>
             <div class="row" style="width: 300px; height: auto; -webkit-align-items: center; margin-right: 0px; margin-left: 25px" :style="pIPTextColour()">
               <div class="row"></div>
-              True Path:&nbsp;&thinsp;
+              Instructions:&nbsp;&thinsp;
               <meter :max="getScoreLimit()" min=0
                      :value="getScore(player.id).trueScore"
                      :high="getScoreLimit() * 0.75"
