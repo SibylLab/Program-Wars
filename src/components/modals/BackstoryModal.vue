fb1cded41439d1844e39aeb656bb3eaf2570591e
strinsberg <stevendkom@gmail.com>
2020-05-26 14:53:01 -0600

    cleaned up the menu modals a bit. The rules modal is long and a bit gnarly so it is probably still in need of some help.

--- src/components/modals/BackstoryModal.vue
+++ src/components/modals/BackstoryModal.vue
@@ -1,39 +1,46 @@
 <template>
   <div>
-    <div class="modal-dialog modal-lg" role="document" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg" role="document"
+        data-backdrop="static" data-keyboard="false">
       <div class="modal-content" style="border-radius: 30px">
+
         <div class="modal-header" style="border: none"> 
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
           </button>
           <h3 class="modal-title"> In the not too distant future... </h3>
         </div>
+
         <div class="modal-body">
-          <div class="container col-lg-12">
-            <div class="row">
-              <p style="font-size: 24px"> When artificial intelligence surpassed human intelligence, it didn’t take long before the robots
-              rose up and enslaved humanity. We fought back but we were no match for their coding
-              abilities. <br> <br>  We now serve the robots and do their bidding. They force us to do their unwanted jobs while
-              they have all sorts of cool coding competitions. After a life of oppression, you have joined the
-              human resistance. The resistance has developed a highly efficient method of coding that allows
-              humans to enter the coding competitions without the robots suspecting. You are now ready to
-              take the fight to the robots and save humanity! </p>
-            </div>
-          </div>
+          <p style="font-size: 24px">
+            When artificial intelligence surpassed human intelligence,
+            it didn’t take long before the robots rose up and enslaved humanity.
+            We fought back but we were no match for their coding abilities.
+            <br>
+            <br>
+            We now serve the robots and do their bidding. They force us to do
+            their unwanted jobs while they have all sorts of cool coding
+            competitions. After a life of oppression, you have joined the human
+            resistance. The resistance has developed a highly efficient method
+            of coding that allows humans to enter the coding competitions without
+            the robots suspecting. You are now ready to take the fight to the
+            robots and save humanity!
+          </p>
         </div>
+
         <div class="modal-footer" style="border: none">
-          <button class="btn btn-default" data-dismiss="modal" style="float: right; margin: 5px;"> Let's go! </button>
+          <button class="btn btn-default" data-dismiss="modal"
+              style="float: right; margin: 5px;"> Let's go! </button>
         </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  /**
-   * The Modal to display the backstory when the user first loads the webpage
-   */
-
-  export default {
-  }
+/**
+ * Modal for displaying the games backstory.
+ */
+export default {}
 </script>
