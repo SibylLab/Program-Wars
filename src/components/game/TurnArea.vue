9b36a15a90e8fe8b7d99f910b5f8ca2fd47ca776
strinsberg <stevendkom@gmail.com>
2020-06-03 16:13:37 -0600

    Added a generic info popup that can be used to create information buttons with popups containing info. It uses the vue slot so that it can be given information in HTML from the parent. This will make it easier to style as desired. The idea is to have concise information in here. Then there is a button to take the player to the rules modal if they need more detailed info.

--- src/components/game/TurnArea.vue
+++ src/components/game/TurnArea.vue
@@ -6,6 +6,18 @@
     REDRAW
   </button>
 
+  <div id="info">
+    <info-popup>
+      <h3 style="margin: 0">Turn Area</h3>
+      <p>Play instruction cards by dragging them to your play area.</p>
+      <p>Other cards can be played on stacks with the top card higlighted.</p>
+      <p>Special cards will tell you where you can play them. You can activate or play a special card by clicking the button that appears over it when it is useable.</p>
+      <p>Any card can be discarded by clicking the trash icon in the left corner when the card is selected</p>
+      <p>You can discard your entire hand if you don't like your options. You will draw a fresh new hand to replace it.</p>
+      <p>Any of the above actions will end your turn.</p>
+    </info-popup>
+  </div>
+
   <div id="cards">
     <ul>
       <li v-for="card in getCurrentPlayerHand.cards" v-bind:key="card.id">
@@ -22,9 +34,10 @@
 
 
 <script>
-import {bus} from '@/components/shared/Bus'
+import InfoPopup from '@/components/shared/InfoPopup'
 import TurnAreaCard from '@/components/game/TurnAreaCard'
 import MessageBox from '@/components/game/MessageBox'
+import {bus} from '@/components/shared/Bus'
 import {mapGetters, mapMutations, mapState, mapActions} from 'vuex'
 
 export default {
@@ -35,6 +48,7 @@ export default {
     }
   },
   components: {
+    'info-popup': InfoPopup,
     'message-box': MessageBox,
     'turn-area-card': TurnAreaCard
   },
@@ -72,6 +86,12 @@ export default {
   left: 0px;
   width: 100%;
   height: 100%;
+}
+
+#info {
+  position: absolute;
+  right: 38%;
+  top: 1.5%;
 }
 
 #cards {
---
+++ src/components/shared/InfoPopup.vue
@@ -0,0 +1,63 @@
+<template>
+<div id="info-component">
+  <input v-if="!active" type='image' id='info-button' src='static/miscIcons/info.png'
+      v-on:click="active = true">
+
+  <div id="info-popup" v-if="active">
+    <slot></slot>
+    <button class="btn btn-sm btn-info action" v-on:click="openRules">
+      More Info
+    </button>
+    <button class="btn btn-sm btn-primary action" v-on:click="active = false">
+      Close
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'info-popup',
+  data () {
+    return {
+      active: true
+    }
+  },
+  methods: {
+    openRules () {
+      this.active = false
+      $('#rulesModal').modal('show')
+    }
+  }
+}
+</script>
+
+<style scoped>
+#info-component {
+  position: relative;
+  z-index: 1000;
+}
+
+#info-popup {
+  min-width: 90px;
+  max-width: 500px;
+  height: auto;
+  border: ridge grey 5px;
+  border-radius: 10px;
+  background-color: white;
+  color: black;
+  padding: 10px;
+  text-align: left;
+  font-size: 15px;
+}
+
+#info-button {
+  width: 20px;
+  height: 20px;
+}
+
+.action {
+  display: inline-block;
+  margin: 5px;
+}
+</style>
