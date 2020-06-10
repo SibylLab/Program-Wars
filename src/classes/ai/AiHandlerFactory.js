1344254d9a44c8ac3b84b2df28988d82ad5c38b3
strinsberg <stevendkom@gmail.com>
2020-06-10 15:02:40 -0600

    Added a very basic factory that will eventually return different types of AI handlers.

---
+++ src/classes/ai/AiHandlerFactory.js
@@ -0,0 +1,26 @@
+/**
+ * @file RedrawAction.js file
+ * @author Steven on 2020-06-10
+ */
+
+import AiHandler from '@/classes/ai/AiHandler'
+import RedrawAction from '@/classes/ai/RedrawAction'
+
+/**
+ * A factory to create different types of AiHandlers.
+ */
+export default class AiHandlerFactory {
+  /**
+   * @constructor RedrawAction
+   */
+  constructor () {}
+
+  /**
+   * Create and return an AiHandler of the given type for the given player.
+   */
+  newHandler (type, player) {
+    type // is unused for now
+    return new AiHandler(player)
+  }
+}
+
