178200f2781c4d10517de35f9ac45c38a792fd23
strinsberg <stevendkom@gmail.com>
2020-06-13 15:22:14 -0600

    Made winners modal larger so it is more readable in four player games.

--- src/components/modals/WinnerModal.vue
+++ src/components/modals/WinnerModal.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content" style="border-radius: 30px">
 
       <div class="modal-header" style="padding-bottom: 0;">
