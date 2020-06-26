6a24cd7bd21a6e2392dcd353a717fd10dc5e6f32
strinsberg <stevendkom@gmail.com>
2020-06-26 10:44:05 -0600

    removed the identifier image that shows you wich of your cards is being mimicked.

--- src/components/game/TurnAreaCard.vue
+++ src/components/game/TurnAreaCard.vue
@@ -13,8 +13,6 @@
          v-on:click="discard">
 
       <effect-card-popup></effect-card-popup>
-      <img v-if="card.isMimic" src="static/cardImages/effects/TROJAN.png" style="width: 30px; height: auto;">
-
     </div>
   </div> 
 
