e40a6e630e3f2c320b24e667762c196d8b9d7b41
strinsberg <stevendkom@gmail.com>
2020-07-15 11:13:14 -0600

    Started adding player data and functions for requirements to the requirements page and state.

--- src/classes/states/RequirementsState.js
+++ src/classes/states/RequirementsState.js
@@ -1,13 +1,40 @@
 import Player2 from '@/classes/game/Player2'
+import ReqFactory from '@/classes/agile/ReqFactory'
+import {requirements} from '@/data/requirements'
 
 export default class RequirementsState {
   constructor (playerList) {
-    this.requirements = ["nothing yet"]
     this.players = this.addPlayers(playerList)
+    this.playerNum = 0
+    this.reqFactory = new ReqFactory()
   }
 
   addPlayers (playerList) {
     let id = 0
-    this.players = playerList.map(p => new Player2(id++, p.name, p.isAI))
+    return playerList.map(p => new Player2(id++, p.name, p.isAI))
+  }
+
+  currentPlayer () {
+    return this.players[this.playerNum]
+  }
+
+  nextPlayer () {
+    this.playerNum++
+
+    if (this.playerNum === this.players.length) {
+      return undefined
+    } else if (this.currentPlayer.isAI()) {
+      this.currentPlayer.requirement = this.reqFactory('DRY')
+      this.nextPlayer()
+    }
+    return this.currentPlayer()
+  }
+
+  requirementNames () {
+    return Object.keys(requirements)
+  }
+
+  requirementDetails (name) {
+    return requirements[name]
   }
 }
--- src/pages/Requirements.vue
+++ src/pages/Requirements.vue
@@ -2,7 +2,17 @@
 <div id="requirements">
   <page-header/>
 
-  <h4 id="title">Requirements Phase: "PLAYER" Select Your Requirements</h4>
+  <h4 class="title centered">Phase 1: Requirements</h4>
+
+  <div id="phase-info" class="centered">
+    <h4>
+      <div class="name">{{ pageState.currentPlayer().name.toUpperCase() }}</div>
+      Select Your Requirement
+    </h4>
+  </div>
+
+  <!-- Component for listing a bunch of cards, title and cards prop? -->
+  <!-- list requriement details -->
 
 </div>
 </template>
@@ -29,11 +39,32 @@ export default {
   height: 100%;
 }
 
-#title {
+#phase-info {
   position: absolute;
   top: 40px;
-  width: 100%;
-  height: 40px;
+}
+
+.name {
+  display: inline;
+  color: red;
+}
+
+
+.title {
+  position: absolute;
+  width: 50%;
+  z-index: 20;
+  color: white;
+}
+
+.centered {
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+h4 {
   margin: 0;
   margin-top: 10px;
 }
