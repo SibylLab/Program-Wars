f0f60e0357f28ceefb86d19896f7e7dd0899b282
strinsberg <stevendkom@gmail.com>
2020-07-30 08:46:07 -0600

    Simplified and cleaned up the new effect notification popup for scan and trojans.

--- src/components/game/NewEffectNotifications.vue
+++ src/components/game/NewEffectNotifications.vue
@@ -1,18 +1,6 @@
 <template>
 <div id="effect-notifications" v-if="showing">
-  <h1 style="color: black; margin-top: 20px;">{{ titleText }}</h1>
-
-  <div style="display: inline; position: relative;">
-    <img id="left-image" :src="leftImage" class="shadow">
-    <img v-if="block" src="static/miscIcons/noIcon.png" class="no-symbol">
-  </div>
-
-  <img id="arrow" src="static/miscIcons/arrow.png">
-
-  <div style="display: inline; position: relative;">
-    <img id="right-image" :src="rightImage" class="shadow">
-    <img v-if="clean" src="static/miscIcons/noIcon.png" class="no-symbol">
-  </div>
+  <img class="icon highlight" :src="imagePath">
 </div>
 </template>
 
@@ -35,12 +23,7 @@ export default {
       pageState: this.$store.state.pageState,
       showing: false,
       imagePath: 'static/cardImages/effects/',
-      leftImage: 'static/cardImages/effects/SCAN.png',
-      rightImage: 'static/cardImages/effects/VIRUS.png',
-      block: false,
-      clean: false,
-      timeout: 1500,
-      titleText: 'Empty'
+      timeout: 1250
     }
   },
   computed: {
@@ -48,37 +31,26 @@ export default {
       return this.pageState.players
     }
   },
+  methods: {
+    mimicPlayed () {
+      this.showing = true
+      this.imagePath = 'static/cardImages/effects/TROJAN.png'
+      setTimeout(() => {this.showing = false}, this.timeout)
+    },
+    scanUsed () {
+      this.showing = true
+      this.imagePath = 'static/cardImages/effects/SCAN.png'
+      setTimeout(() => {this.showing = false}, this.timeout)
+    }
+  },
   created () {
-    // Shows notifications when mimic is played
-    bus.$on('mimic-played', (play) => {
-      // we have already replaced the card???
-      // So this will have to be different, but similar
-      this.titleText = "Mimic!"
-      this.showing = true
-      this.block = false
-      this.clean = false
-      if (play.card.isSpecial()) {
-        this.leftImage = this.imagePath + play.card.type + '.png'
-      } else {
-        this.leftImage = this.imagePath + play.card.type + play.card.value + '.png'
-      }
-      // let replaced = play.card.replace()
-      // this.rightImage = this.imagePath + replaced.type + '.png'
-      setTimeout(() => {this.showing = false}, this.timeout)
-    })
-
-    // Shows notification when scan removes an effect
-    bus.$on('scan-used', (type) => {
-      this.titleText = "Scanned!"
-      this.showing = true
-      this.block = false
-      this.clean = true
-      this.leftImage = this.imagePath + 'SCAN.png'
-      this.rightImage = this.imagePath + type + '.png'
-      setTimeout(() => {this.showing = false}, this.timeout)
-    })
+    bus.$on('mimic-played', this.mimicPlayed)
+    bus.$on('scan-used', this.scanUsed)
+  },
+  beforeDestroy () {
+    bus.$off('mimic-played', this.mimicPlayed)
+    bus.$off('scan-used', this.scanUsed)
   }
-  // need to destroy these too
 }
 </script>
 
@@ -87,36 +59,22 @@ export default {
 #effect-notifications {
   position: absolute;
   top: 35%;
-  left: 30%;
-  width: 40%;
-  z-index: 80;
-  background-color: #ffffff;
-  border: ridge grey 6px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 100;
+}
+
+.icon {
+  width: 20%;
+}
+
+.highlight {
+  -webkit-box-shadow: 0 0 24px 40px rgba(255,0,0,1);
+  -moz-box-shadow: 0 0 24px 40px rgba(255,0,0,1);
+  box-shadow: 0 0 24px 40px rgba(255,0,0,1);
   border-radius: 20px;
-}
-
-#left-image {
-  width: 150px;
-  margin: 30px;
-}
-
-#right-image {
-  width: 150px;
-  margin: 30px;
-}
-
-.no-symbol {
-  position: absolute;
-  top: -40px;
-  right: 55px;
-  width: 100px;
-}
-
-.shadow {
-  -webkit-box-shadow: 0 0 24px 24px rgba(0,255,0,1);
-  -moz-box-shadow: 0 0 24px 24px rgba(0,255,0,1);
-  box-shadow: 0 0 24px 24px rgba(255,150,0,1);
-  border-radius: 10px;
 }
 
 </style>
