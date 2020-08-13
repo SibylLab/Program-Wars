1684bf3d94ad6a5e3d158f5bb24421f40d443b73
strinsberg <stevendkom@gmail.com>
2020-08-13 10:59:22 -0600

    Styled the hepl page a little and added a blank component to test the vue-markdown component with.

---
+++ src/components/help/Intro.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  INTRODUCTION
+</div>
+</template>
+
+<script>
+export default {
+  name: "help-intro",
+  components: {
+    //'vue-markdown': vue-markdown
+  }
+}
+</script>
--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,21 +1,69 @@
 <template>
 <div id="help-page">
-  HELP PAGE
+  <!-- nav is not a separate component to make it easy to add new pages
+       and the links to them. -->
+  <div class='side-nav'>
+    <h2> Help Pages </h2>
+    <br><br>
+    <a v-on:click="open('intro')"> Introduction </a>
+  </div>
+
+  <div class='help-content'>
+    <help-intro v-if="isOpen('intro')"/>
+  </div>
+
 </div>
 </template>
 
 <script>
+import Intro from '@/components/help/Intro'
+
 export default {
-  name: 'help-page'
+  name: 'help-page',
+  data () {
+    return {
+      page: 'intro'
+    }
+  },
+  components: {
+    'help-intro': Intro
+  },
+  methods: {
+    open (page) {
+      this.page = page
+    },
+    isOpen (page) {
+      return this.page === page
+    }
+  }
 }
 </script>
 
 <style scoped>
-#help-page {
+.side-nav {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
+  width: 20%;
   height: 100vh;
+  padding: 1rem;
+  color: white;
+  background-color: #333333;
+}
+
+.side-nav a {
+  font-size: 1.5rem;
+}
+
+.side-nav a:hover {
+  color: #0077FF;
+  cursor: pointer;
+}
+
+.help-content {
+  position: absolute;
+  top: 0;
+  left: 20%;
+  width: 80%;
 }
 </style>
