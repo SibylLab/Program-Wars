a31733e11b1b654d9482deda25e75ef85d01faf0
strinsberg <stevendkom@gmail.com>
2020-08-19 15:36:24 -0600

    Added MimicWrapper tests, and started Repeat tests.

---
+++ tests/unit/card/Repeat.spec.js
@@ -0,0 +1,17 @@
+import Repeat from '@/classes/card/Repeat'
+
+describe('Repeat class', () => {
+  test('creating a new repeat card', () => {
+
+  })
+
+  describe('play', () => {
+    test('when the target stack is not complete', () => {
+
+    })
+
+    test('when the there is a mistake and the stack is complete', () => {
+
+    })
+  })
+})
