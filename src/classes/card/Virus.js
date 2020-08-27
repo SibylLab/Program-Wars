9a82917d32bd540fdbfc99beede6b2bb18444592
strinsberg <stevendkom@gmail.com>
2020-08-27 12:21:31 -0600

    Added the last of the card documentation.

--- src/classes/card/Virus.js
+++ src/classes/card/Virus.js
@@ -1,10 +1,29 @@
 import Card from '@/classes/card/Card'
 
-export default class Virus extends Card {
+/**
+ * Class for the Virus card.
+ * @extends Card
+ */
+class Virus extends Card {
+  /**
+   * Creates a new Virus card.
+   * @param {Deck} deck - The deck the card is in.
+   */
   constructor (deck) {
     super(0, 'VIRUS', deck, Card.imgPath('virus'))
   }
 
+  /**
+   * Plays a Virus card on a given stack.
+   *
+   * If the top of the stack is already a Virus or the stack owner is
+   * protected from viruses then the virus card will be discarded.
+   * If the stack owner has a `scan` effect active then the effect will be removed
+   * and the virus card discarded.
+   *
+   * @param {Object} playInfo - Information about how the card was played.
+   * @param {Stack} playInfo.stack - The stack the card was played on.
+   */
   play (playInfo) {
     if (playInfo.stack.player.helpedBy('SCAN')) {
       this._blockedByScan(playInfo)
@@ -18,3 +37,4 @@ export default class Virus extends Card {
   }
 }
 
+export default Virus;
