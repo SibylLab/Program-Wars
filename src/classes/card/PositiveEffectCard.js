9f7cef7f2053a4acad67df68db000aec7f1a87a0
strinsberg <stevendkom@gmail.com>
2020-08-27 10:35:53 -0600

    Added documentation for more cards.

--- src/classes/card/PositiveEffectCard.js
+++ src/classes/card/PositiveEffectCard.js
@@ -1,11 +1,31 @@
 import Card from '@/classes/card/Card'
 import EffectFactory from '@/classes/statusEffect/EffectFactory'
 
-export default class PositiveEffectCard extends Card {
+/**
+ * Class for any card that only adds a poitive StatusEffect to a player.
+ *
+ * i.e. ANTIVIRUS and FIREWALL. **Not** for SCAN.
+ * @extends Card
+ */
+class PositiveEffectCard extends Card {
+  /**
+   * Creates a new PositiveEffectCard for a given card type.
+   * @param {string} type - The type of effect.
+   * @param {Deck} deck - The deck the card is in.
+   */
   constructor (type, deck) {
     super(0, type, deck, Card.imgPath(type.toLowerCase()))
   }
 
+  /**
+   * Plays the positive effect on a target player.
+   *
+   * Effects like Antivirus and Firewall will remove effects they block.
+   * If the player is already helped by the effect the card will simply be discarded.
+   *
+   * @param {Object} playInfo - Information about how the card was played.
+   * @param {Player} playInfo.target - The player the effect is being added to.
+   */
   play ({ target }) {
     if (!target.helpedBy(this.type)) {
       const fact = new EffectFactory(target)
@@ -24,3 +44,4 @@ export default class PositiveEffectCard extends Card {
   }
 }
 
+export default PositiveEffectCard;
