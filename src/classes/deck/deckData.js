6ef90ca49399adb8fb31abf9a6785e77da4e9aa8
strinsberg <stevendkom@gmail.com>
2020-08-27 15:58:07 -0600

    Added documentation to a few more classes. Adjusted a function in Deck that returned null to just return undefined as it is more consistent with other functions when they have no good return value.

--- src/classes/deck/deckData.js
+++ src/classes/deck/deckData.js
@@ -1,3 +1,67 @@
+/**
+ * @module deckData
+ *
+ * @fileOverview
+ * Contains information on what cards to use in a deck.
+ *
+ * I ended up with a layout that did not work well for proper documentation
+ * so some descriptions of the file layout is given here in markdown for API
+ * documentation. More info on what is in each deck can be found by looking in
+ * the source code.
+ *
+ * Each deck is an object setup like so:
+ * ```
+ * {
+ *   base  // The cards types used in the base for the deck
+ *   extra // The cards to include along with the base deck
+ * }
+ * ```
+ *
+ * The card types objects are set up as objects like so:
+ * ```
+ * {
+ *   type // The type of the card
+ *   val  // The value of the card
+ *   num  // The number of cards of this type to add
+ * }
+ * ```
+ *
+ * The levels for each mode are a list of 4 items that look like so:
+ * ```
+ * {
+ *   id          // the name of the deck (can be used to index) deckData.<mode>[id] for deck data
+ *   name        // The display name for the level
+ *   description // A list of safety and attack cards added in the level
+ * }
+ * ```
+ *
+ * Export object looks like this:
+ * ```
+ * {
+ *   beginner:       // a collection of beginner mode data
+ *   {
+ *     malware1      // The malware1 deck info i.e. { base, extra }
+ *     malware2      // The malware2 deck info
+ *     hack1         // The hack1 deck info
+ *     hack2         // The hack2 deck info
+ *     default       // The default deck
+ *     levels        // List of decks, names and descriptions
+ *   },
+ *   standard:       // a collection of standard deck data
+ *   {
+ *     stdMalware    // The only malware deck info i.e. { base, extra }
+ *     stdHack       // The only hacks deck info
+ *     stdCombined1  // Deck info for 2 malware and 2 hack cards
+ *     stdCombined2  // Deck info for a different set of 2 malware and 2 hack cards
+ *     default       // The default deck
+ *     levels        // List of decks, names and descriptions
+ *   }
+ * }
+ * ```
+ *
+ * @exports deckData
+ */
+
 // make an object to record the type value and number of a card that
 // will be in a deck.
 function makeType (type, val, num) {
@@ -6,6 +70,7 @@ function makeType (type, val, num) {
 
 // Begginer Decks ////////////////////////////////////////////////////////////
 
+// Base cards to add to each beginner deck
 const beginnerBase = [
   makeType('INSTRUCTION', 1, 8),
   makeType('INSTRUCTION', 2, 14),
@@ -21,6 +86,7 @@ const beginnerBase = [
   makeType('SORT', 0, 2)
 ]
 
+// Sets of special cards to add to the beginner base to make a full beginner deck
 const b1Special = [
   makeType('SPYWARE', 0, 3),
   makeType('RANSOM', 0, 3),
@@ -45,6 +111,7 @@ const b4Special = [
   makeType('FIREWALL', 0, 1)
 ]
 
+// Full deck objects containing lists of base and extra cards
 const malware1 = {
   base: beginnerBase,
   extra: b1Special
@@ -65,8 +132,10 @@ const hack2 = {
   extra: b4Special
 }
 
+// Default deck for beginner mode
 const beginnerDefault = malware1
 
+// List of level descriptions and an id for the deck they use
 const beginnerLevels = [
   {
     id: 'malware1',
@@ -92,6 +161,7 @@ const beginnerLevels = [
 
 // Standard Decks ////////////////////////////////////////////////////////////
 
+// base card types
 const standardBase = [
   makeType('INSTRUCTION', 1, 6),
   makeType('INSTRUCTION', 2, 14),
@@ -110,6 +180,7 @@ const standardBase = [
   makeType('SORT', 0, 3)
 ]
 
+// aditional card types
 const std1Special = [
   makeType('SPYWARE', 0, 2),
   makeType('RANSOM', 0, 2),
@@ -144,6 +215,7 @@ const std4Special = [
   makeType('ANTIVIRUS', 0, 1)
 ]
 
+// Standard deck setups
 const stdMalware = {
   base: standardBase,
   extra: std1Special
@@ -164,8 +236,10 @@ const stdCombined2 = {
   extra: std4Special
 }
 
+// Default for standard decks
 const standardDefault = stdMalware
 
+// list of standard levels
 const standardLevels = [
   {
     id: 'stdMalware',
