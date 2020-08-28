4c51e63cec134d1914070b933c05985c37fc21b3
strinsberg <stevendkom@gmail.com>
2020-08-28 11:24:01 -0600

    Added more documentation for StatusEffect classes. Made a method in StatusEffects 'private'.

--- src/classes/statusEffect/EffectFactory.js
+++ src/classes/statusEffect/EffectFactory.js
@@ -5,35 +5,72 @@ import AttackWithBonus from '@/classes/statusEffect/AttackWithBonus'
 import InvisibleBonusEffect from '@/classes/statusEffect/InvisibleBonusEffect'
 import SqlEffect from '@/classes/statusEffect/SqlEffect'
 
+// Map from effect types to their penalty points (if they have them)
 const penalties = {
   'RANSOM': -10, 'SQL_INJECTION': -2
 }
 
+// Map from effect types to their bonus points (if they have them)
 const bonuses = {
   'RANSOM': 10
 }
 
+// Map from effect types to how many turns they last (if they are temporary)
 // spyware adds +1 as it updates once before you can use its effect
 const turns = {
   'SPYWARE': 6, 'STACK_OVERFLOW': 2, 'STACK_UNDERFLOW': 2, 'DDOS': 3,
   'REDRAW_CD': 3
 }
 
-export default class EffectFactory {
+/**
+ * Factory class to create statusEffects
+ */
+class EffectFactory {
+  /**
+   * Creates a new effect factory.
+   * @param {Player} player - The player the effects will be added to.
+   */
   constructor (player) {
     this.player = player
   }
 
+  /**
+   * Creates a new statusEffect for effects that do not come from cards.
+   *
+   * @param {String} type - The type of the effect.
+   * @param {int} extraTurns - Additional turns to add to the effect length.
+   * If the effect should be permanent this is ignored.
+   * @param {bool} [hasImage=true] - True if the effect has an image.
+   * See {@link StatusEffect}.
+   * @return {StatusEffect} The new status effect.
+   */
   newEffect (type, extraTurns, hasImage = true) {
     const turns = this._getTurns(type, extraTurns)
     return new StatusEffect(type, this.player, turns, hasImage)
   }
 
+  /**
+   * Creates a positive StatusEffect that comes from a card like `antivirus`.
+   *
+   * @param {Card} card - The card that creates the effect.
+   * @param {int} extraTurns - The additional turns to add to the effect duration.
+   * If the effect should be permanent this is ignored.
+   * @return {EffectWithCard} The new status effect.
+   */
   newPositiveFromCard (card, extraTurns) {
     const turns = this._getTurns(card.type, extraTurns)
     return new EffectWithCard(card, this.player, turns)
   }
 
+  /**
+   * Creates a new negative StatusEffect that comes from an attack card like `ransom`.
+   *
+   * @param {Card} card - The card that creates the effect.
+   * @param {int} extraTurns - The additional turns to add to the effect duration.
+   * If the effect should be permanent this is ignored.
+   * @param {Player} attacker - The player that played the card.
+   * @return {CyberAttack} The new status effect.
+   */
   newNegativeFromCard (card, extraTurns, attacker) {
     const turns = this._getTurns(card.type, extraTurns)
     const penalty = this._getPenalty(card.type)
@@ -49,14 +86,29 @@ export default class EffectFactory {
     }
   }
 
+  /**
+   * Get the penalty amount for the given type.
+   * Returns 0 if the type has no penalty.
+   * @private
+   */
   _getPenalty (type) {
     return type in penalties ? penalties[type] : 0
   }
 
+  /**
+   * Get the bonus amount for the given type.
+   * Returns 0 if the type has no bonus.
+   * @private
+   */
   _getBonus (type) {
     return type in bonuses ? bonuses[type] : 0
   }
 
+  /**
+   * Get the number of turns for the effect.
+   * Returns -1 if the effect is permanent, even if there are extra turns given.
+   * @private
+   */
   _getTurns (type, extraTurns) {
     if (type in turns) {
       return turns[type] + extraTurns
@@ -65,3 +117,5 @@ export default class EffectFactory {
     }
   }
 }
+
+export default EffectFactory;
--- src/classes/statusEffect/SqlEffect.js
+++ src/classes/statusEffect/SqlEffect.js
@@ -1,15 +1,33 @@
 import CyberAttack from '@/classes/statusEffect/CyberAttack'
 
-export default class SqlEffect extends CyberAttack {
+/**
+ * Class for a StatusEffect specifically for `SQL_INJECTION`.
+ * @prop {MethodStack} method - The method stack being affected.
+ * @extends CyberAttack
+ */
+class SqlEffect extends CyberAttack {
+  /**
+   * Creates a new SqlEffect and adds its penalty as an adjustment to the
+   * target players method.
+   * @param {Card} card - The card that created the attack.
+   * @param {Player} player - The player the effect is added to.
+   * @param {int} turns - The turns the effect will last, -1 for permanent.
+   * @param {Player} attacker - The player that played the attack.
+   * @param {int} penalty - The value of the penalty.
+   */
   constructor (card, player, turns, attacker, penalty) {
     super(card, player, turns, attacker, penalty)
     this.method = this.player.playField.method
     this.method.adjustment += this.penalty
   }
 
+  /**
+   * Destroy the effect and remove the adjustemnt from the affected method.
+   */
   destroy () {
     super.destroy()
     this.method.adjustment -= this.penalty
   }
 }
 
+export default SqlEffect;
