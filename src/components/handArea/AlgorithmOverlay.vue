ac3cb137acb19ade762806a3be3decddef122114
strinsberg <stevendkom@gmail.com>
2020-08-31 11:34:53 -0600

    Addded documentation for some of the hand area components.

--- src/components/handArea/AlgorithmOverlay.vue
+++ src/components/handArea/AlgorithmOverlay.vue
@@ -1,5 +1,10 @@
 <template>
 <div id="algorithm-overlay">
+  <!-- CLicking play on a card that is not a mimic will set the card type so
+       that isActive(type) will return true for that type. This will show the
+       component to allow the player to play that card type. Adding new
+       algorithm cards should include adding a new component here with
+       v-if="isActive("newCardType"). -->
   <search v-if="isActive('SEARCH')" :deck="game.getDeck()"
     :card="card" :cardOwner="owner"/> 
   <sort v-if="isActive('SORT')" :deck="game.getDeck()"
@@ -19,6 +24,20 @@ import Search from '@/components/shared/Search'
 import Sort from '@/components/shared/Sort'
 import { mapGetters } from 'vuex'
 
+/**
+ * The overlay that appears over a selected algorithm card with a button to
+ * activate the card.
+ *
+ * @vue-prop {Card} card - The algorithm card to overlay.
+ * @vue-prop {Player} owner - The player that the card belongs to.
+ * @vue-data {string} [active=null] - The type of the active card. Used to decide if
+ * a card specific component should be shown to play that card. If its value is
+ * null or a card type that does not have a component above then nothing will change.
+ *
+ * @vue-computed {bool} canPlay - True if the card can be played.
+ * @vue-computed {string} titleText - The text to show at the top of the overlay.
+ * If the card can be played it is the card type, otherwise "Not Available".
+ */
 export default {
   name: 'algorithm-overlay',
   props: ['card', 'owner'],
@@ -41,9 +60,21 @@ export default {
     }
   },
   methods: {
+    /**
+     * Checks if the given type is the active type.
+     * @param {string} type - The type to check.
+     * @return {bool} True if the type is the active type, false otherwise.
+     */
     isActive (type) {
       return this.active === type
     },
+    /**
+     * Plays the algorithm card.
+
+     * If the card is not a mimic the data will change to show the component
+     * for playing the active card type. If the card is a mimic then the card
+     * will be played as a special card which will resolve the mimic.
+     */
     play () {
       if (!this.card.isMimic) {
         this.active = this.card.type
--- src/components/handArea/PlayerHand.vue
+++ src/components/handArea/PlayerHand.vue
@@ -27,13 +27,18 @@ import { isSpecial, isAlgorithm } from '@/classes/card/cardData'
 import { bus } from '@/components/shared/Bus'
 import { mapGetters } from 'vuex'
 
+/**
+ * Displays the contents of a players hand.
+ *
+ * @vue-prop {Player} player - The player whose hand we want to display.
+ * @vue-data {bool} update - When this changes value the component will redraw itself.
+ */
 export default {
   name: 'player-hand',
   props: ['player'],
   data () {
     return {
       update: true,
-      active: false
     }
   },
   components: {
@@ -45,37 +50,93 @@ export default {
     ...mapGetters(['game'])
   },
   methods: {
+    /**
+     * Checks if the given card is the currently selected card.
+     *
+     * Is always false if the player is an AI.
+     * @param {Card} card - The card to check.
+     * @return {bool} True if the card is the current card, false otherwise.
+     */
     isCurrentCard (card) {
       return this.game.currentCard === card && !this.player.isAI
     },
+    /**
+     * Helper to check if given card type === 'SCAN'
+     * @param {string} type - The card type to check.
+     * @return {bool} True if the card type is 'SCAN', false otherwise.
+     */
     isScan (type) {
       return type === 'SCAN'
     },
+    /**
+     * Wrapper for cardData.isAlgorithm as we cannot access that function drectly
+     * in the component template.
+     * @param {string} type - The card type to check.
+     * @return {bool} True if the card type is an algorithm type, false otherwise.
+     */
     isAlgorithm (type) {
       return isAlgorithm(type)
     },
+    /**
+     * Checks if the given card type is any special type other than scan or algorithm.
+     * @param {string} type - The card type to check.
+     * @return {bool} True if the card type is a special type other than a scan or
+     * an algorithm, false otherwise.
+     */
     isOther (type) {
       return isSpecial(type) && !this.isScan(type) && !this.isAlgorithm(type)
     },
+    /**
+     * Checks if the given card can be dragged.
+     *
+     * Only cards that are played on stacks or the play field are draggable. Also,
+     * the card must be playable to be able to be dragged.
+     * @param {Card} card - The card to check.
+     * @return {bool} True if the card can be dragged, false otherwise.
+     */
     canDrag (card) {
       return !isSpecial(card.type) && this.player.canPlay(card)
     },
+    /**
+     * Checks whether or not a a given card should have an overlay.
+     * @param {Card} card - The card to check.
+     * @return {bool} True if the card is the selected card and it should show
+     * an overlay.
+     */
     showOverlay (card) {
       return this.isCurrentCard(card) && isSpecial(card.type)
           && this.player.canPlay(card.type)
     },
+    /**
+     * Gets the image path for the given card.
+     * @param {Card} card - The card to get the image path for.
+     * @return {string} The path to the appropriate image for the card, or back
+     * of card if the current player is an AI.
+     */
     cardImage (card) {
       if (this.game.currentPlayer().isAI) {
         return 'static/cardImages/backOfCard.png'
       }
       return card.image
     },
+    /**
+     * Determines the CSS class for the higlight to show around the card.
+     * @param {Card} card - The card to check.
+     * @return {string} The CSS class for putting a highlight around the card.
+     * Will be '' if no shadow should be shown.
+     */
     cardShadow (card) {
       if (!this.isCurrentCard(card)) {
         return ''
       }
       return this.player.canPlay(card.type) ? 'play' : 'no-play'
     },
+    /**
+     * Selects the given card and updates the component.
+     *
+     * Emits a `select-card` event indicating a new card was selected.
+     * @param {Card} card - The card to select.
+     */
     select (card) {
       if (!this.isCurrentCard(card)) {
         this.game.setCurrentCard(card)
@@ -83,6 +144,10 @@ export default {
         bus.$emit('select-card')
       }
     },
+    /**
+     * Takes a discard card turn for the player, discarding the given card.
+     * @param {Card} card - The card to discard.
+     */
     discard (card) {
       if (!this.player.isAi) {
         this.game.takeTurn({
@@ -92,6 +157,17 @@ export default {
         })
       }
     },
+    /**
+     * Sets up an event for dragging a given card.
+     *
+     * Adds the following data members to the `event`. They can be retrieved
+     * in other places with `event.dataTransfer.getData('member-name')`.
+     * - `cardId` = the id of the given card.
+     * - `playerId` = the id of the player who owns the card.
+     *
+     * @param {event} event - The event that is being added to.
+     * @param {Card} card - The card being dragged.
+     */
     startDrag (event, card) {
       event.dataTransfer.dropEffect = 'move'
       event.dataTransfer.effectAllowed = 'move'
